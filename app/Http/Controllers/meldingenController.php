58bfff6260a7d8266b50d1fc44e9a2e170dd4a10
Luckynotam <144550173+Luckynotam@users.noreply.github.com>
2026-02-23 13:49:42 +0100

    Added the rest of the references to the controller

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -19,7 +19,7 @@ echo $attractie . " / " . $type." / ". $capaciteit . " / " . $prioriteit. " / ".
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, capaciteit, melder) VALUES (:attractie, :capaciteit, :melder)";
+$query = "INSERT INTO meldingen (attractie, type,  capaciteit, prioriteit, melder, overige_info) VALUES (:attractie, :type, :capaciteit, :prioriteit,  :melder, :overig)";
 
 //3. Prepare
 $statement = $conn->prepare($query);
@@ -27,7 +27,10 @@ $statement = $conn->prepare($query);
 //4. Execute
 $statement->execute([
     ":attractie"=>$attractie,
+    ":type"=>$type,
     ":capaciteit"=>$capaciteit,
+    ":prioriteit"=>$prioriteit
     ":melder"=>$melder,
+    ":overig"=>$overig,
 ]);
 
