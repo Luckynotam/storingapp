0e0c98c7cedb0ddb208137b41c176762a2876faa
Luckynotam <l.w.t.maton@gmail.com>
2026-03-16 13:36:23 +0100

    Updated login.php to include name and id attributes for the input

--- login.php
+++ login.php
@@ -17,12 +17,12 @@
         <form action="app/Http/Controllers/loginController.php" method="POST">
             <div class="form-group">
                 <label for="username">Gebruikers naam:</label>
-                <input type="text">
+                <input type="text" name="username" id="username">
 
             </div>
             <div class="form-group">
                 <label for="password">Wachtwoord:</label>
-                <input type="password">
+                <input type="password" name="password" id="password">
             </div>
             <div class="form-group">
                 <input type="submit" value="inloggen">
