69f245c1b161effa3f7c9fbe0b1165939d328e7a
Luckynotam <l.w.t.maton@gmail.com>
2026-03-16 12:21:39 +0100

    Created login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>StoringApp</title>
+    <?php require_once 'resources/views/components/head.php'; ?>
+    <link rel="stylesheet" href="public_html/css/main.css">
+    <link rel="stylesheet" href="public_html/css/normalize.css">
+</head>
+
+<body>
+
+    <?php require_once 'resources/views/components/header.php'; ?>
+
+    <div class="container home">
+
+        
+
+    </div>
+
+</body>
+
+</html>
